adf38174fab9a0f0645a80fc0bd33a63e650ce58
HYDRO777 <74252819+HYDRO777@users.noreply.github.com>
2022-03-10 18:47:25 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,49 @@
+img = ""
+status7 = ""
+objects = [];
+
+function preload() {
+    img = loadImage("ball.jfif")
+}
+
+function setup() {
+    canvas = createCanvas(774, 424);
+    canvas.center();
+    detector = ml5.objectDetector('cocossd', modelLoaded)
+    document.getElementById("detectionstatus").innerHTML = "Status = Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("modelloaded");
+    detector.detect(img, gotResults);
+    status7 = true;
+}
+
+function draw() {
+    image(img, 0, 0, 774, 424);
+    if (status7 != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("detectionstatus").innerHTML = "Status = Objects Detected";
+            percentage = floor(objects[i].confidence * 100);
+            fill("red");
+            text(objects[i].label + "" + percentage + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("red")
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+        document.getElementById("detectioncount").innerHTML = "Number of Objects is =" + objects.length;
+    }
+}
+
+function bye(){
+    window.location = "home.html"
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function home1(){
+    window.location = "watch.html"
+}
+function home2(){
+    window.location = "ipad.html"
+}
+function home3(){
+    window.location = "pc.html"
+}
+function home4(){
+    window.location = "keyboard.html"
+}
+function home5(){
+    window.location = "ball.html"
+}
